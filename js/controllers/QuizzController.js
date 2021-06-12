5dbfc38017befa840b22adc0c6c65c153bf8eb3f
jialn5210 <jialongye5210@gmail.com>
2021-06-12 17:44:57 +0100

    ...

--- js/controllers/QuizzController.js
+++ js/controllers/QuizzController.js
@@ -5,29 +5,33 @@ export default class QuizzController {
         this.QuizzModel = new QuizzModel();
     }
 
-    createQuizz(pergunta, opcao1, opcao2, opcao3 , opcao4, resposta, pontos , nivel, imagem) {
-        this.QuizzModel.create(pergunta, opcao1, opcao2, opcao3 , opcao4, resposta, pontos , nivel, imagem);
+    createQuizz(pergunta, opcao1, opcao2, opcao3, opcao4, resposta, pontos, nivel, imagem) {
+        this.QuizzModel.create(pergunta, opcao1, opcao2, opcao3, opcao4, resposta, pontos, nivel, imagem);
     }
 
-   
+
 
     setCurrentQuizz(id) {
         this.QuizzModel.setCurrentQuizz(id)
     }
     getCurrentQuizz() {
         return this.QuizzModel.getCurrentQuizz()
-    } 
+    }
 
 
 
-    getQuizzes(level=sessionStorage.getItem('level')){
-        
+    getQuizzes(level) {
+
         const quizzes = this.QuizzModel.getQuizzes()
-        let filteredQuizzes=[]
-        for(const question of quizzes){
-            if(question.nivel == level);
-            filteredQuizzes.push(question)
+        let filteredQuizzes = []
+        for (const question of quizzes) {
+            if (question.nivel == level);{
+                filteredQuizzes.push(question)
+            }
+            
         }
+
+
         return filteredQuizzes
 
     }
@@ -38,8 +42,8 @@ export default class QuizzController {
 
 
 
-    editQuizz(pergunta, opcao1, opcao2, opcao3 , opcao4, resposta, pontos , nivel, imagem) {
-        this.QuizzModel.editQuizz(pergunta, opcao1, opcao2, opcao3 , opcao4, resposta, pontos , nivel, imagem)
-        
+    editQuizz(pergunta, opcao1, opcao2, opcao3, opcao4, resposta, pontos, nivel, imagem) {
+        this.QuizzModel.editQuizz(pergunta, opcao1, opcao2, opcao3, opcao4, resposta, pontos, nivel, imagem)
+
     }
 }
--- js/views/Question.view.js
+++ js/views/Question.view.js
@@ -7,32 +7,46 @@ export default class QuestionView {
 
         // Catalog
         /*  this.catalog = document.querySelector("#myCatalog") */
-        this.questionName= document.getElementById('txtQuestion')
+        this.questionName = document.getElementById('txtQuestion')
         this.level = sessionStorage.getItem('level')
         this.renderQuestions(this.quizzController.getQuizzes(this.level))
 
-        
+        console.log(this.quizzController.getQuizzes(this.level));
     }
 
 
-    renderQuestions(quizzes = []){
+    renderQuestions(quizzes) {
 
 
-    let result = ''
-    for(const question of quizzes){
-        result += this._generateQuestions(question)
+        let result = ''
+        for (const question of quizzes) {
+            result += this._generateQuestions(question)
+        }
+        this.questionName.innerHTML = result
     }
-     this.questionName.innerHTML = result
-    }
 
 
-    _generateQuestions(question){
+    _generateQuestions(question) {
         let html = `
+        <div class="card" id="question">
         <div class="row">
+
         <div class="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center">
-            <img id="flagGuessCountry" src="${question.imagem}">
+            <img id="cardImg" src="${question.imagem}">
         </div>
-       
+        <div class="col-lg-8 card-title"> 
+          <h2>${question.pergunta}</h2>
+          </div>
+          <input type="radio" name="resposta" id="answer1" value="${question.opcao1}">
+          <label for="answer1">${question.opcao1}</label>
+          <input type="radio" name="resposta" id="answer2" value="${question.opcao2}">
+          <label for="answer2">${question.opcao2}</label>
+        
+          <input type="radio" name="resposta" id="answer3" value="${question.opcao3}">
+          <label for="answer3">${question.opcao3}</label>
+          <input type="radio" name="resposta" id="answer4" value="${question.opcao4}">
+          <label for="answer4">${question.opcao4}</label>
+          </div>
     </div>`
         return html
     }
