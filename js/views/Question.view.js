9358647658389fe45820142c7e8eadeac201d97a
jialn5210 <jialongye5210@gmail.com>
2021-06-12 20:02:28 +0100

    ....

--- js/views/Question.view.js
+++ js/views/Question.view.js
@@ -10,8 +10,10 @@ export default class QuestionView {
         this.questionName = document.getElementById('txtQuestion')
         this.level = sessionStorage.getItem('level')
         this.renderQuestions(this.quizzController.getQuizzes(this.level))
-
+        this.submitBtn = document.getElementById('submit')
         console.log(this.quizzController.getQuizzes(this.level));
+
+        this.checkSubmition()
     }
 
 
@@ -37,18 +39,45 @@ export default class QuestionView {
         <div class="col-lg-8 card-title"> 
           <h2>${question.pergunta}</h2>
           </div>
-          <input type="radio" name="resposta" id="answer1" value="${question.opcao1}">
-          <label for="answer1">${question.opcao1}</label>
-          <input type="radio" name="resposta" id="answer2" value="${question.opcao2}">
-          <label for="answer2">${question.opcao2}</label>
+          <input type="radio" name="${question.id}" id="${question.opcao1}" value="${question.opcao1}">
+          <label for="${question.opcao1}">${question.opcao1}</label>
+          <input type="radio" name="${question.id}" id="${question.opcao2}" value="${question.opcao2}">
+          <label for="${question.opcao2}">${question.opcao2}</label>
         
-          <input type="radio" name="resposta" id="answer3" value="${question.opcao3}">
-          <label for="answer3">${question.opcao3}</label>
-          <input type="radio" name="resposta" id="answer4" value="${question.opcao4}">
-          <label for="answer4">${question.opcao4}</label>
+          <input type="radio" name="${question.id}" id="${question.opcao3}" value="${question.opcao3}">
+          <label for="${question.opcao3}">${question.opcao3}</label>
+          <input type="radio" name="${question.id}" id="${question.opcao4}" value="${question.opcao4}">
+          <label for="${question.opcao4}">${question.opcao4}</label>
           </div>
     </div>`
         return html
     }
 
+
+    checkSubmition() {
+
+        this.submitBtn.addEventListener('click', event => {
+            event.preventDefault();
+
+            let correct_questions = 0;
+            let filteredQuizzes = this.quizzController.getQuizzes(this.level);
+            /* for (let i = 0; i <= filteredQuizzes.length; i++) { */
+
+                for (const question of filteredQuizzes) {
+                    /* alert(question.pergunta) */
+                    const answer = document.querySelector(`input[name="${question.id}"]:checked`)
+                    if (answer.value == question.resposta) {
+                        correct_questions++
+                    }
+                   
+                }
+
+
+          /*   } */
+            console.log(correct_questions * 5);
+
+
+        })
+
+    }
 }
